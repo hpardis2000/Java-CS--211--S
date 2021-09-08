a3fd9f2b1ae65709f4a3c7524c778797fabfa2f8
bpedorya <bpedorya@gmial.com>
2021-09-07 23:33:28 -0700

    DRIVER V3 i Please enter the commit message for your changes. Lines starting

--- BankAccountDriver.java
+++ BankAccountDriver.java
@@ -8,8 +8,8 @@ public class BankAccountDriver {
 		// Test Client Class
 		
 		Client client1=new Client(101,"Ali","San Francisco");
-		Client client2=new Client(102);
-		Client client3=new Client(103);
+		Client client2=new Client(102, "Baba");
+		Client client3=new Client(103, "Johny Cash");
 		Client client4=new Client("Joye","Belmont");
 		System.out.println(client1);
 		System.out.println("Client3.equals(Client2)= " + client3.equals(client2));
@@ -30,44 +30,12 @@ public class BankAccountDriver {
 		// Test automated account number assignment
 
 				Checking checkAcct1 = new Checking(client1.getClientId(), 5500);
-				Savings savingsAcct1 = new Savings(Account.generateAccountNumber(), null, client2.getClientId(), 10000, false);
-				Credit creditAcct1 = new Credit(Account.generateAccountNumber(), null, client3.getClientId(), -777, false);
+				Savings savingsAcct1 = new Savings(client2.getClientId(), 10000, LocalDate.now());
+				Credit creditAcct1 = new Credit(-777.00, client3.getClientId());
 
 				System.out.println(creditAcct1.toString());
 				System.out.println(savingsAcct1.toString());
 				System.out.println(checkAcct1.toString());
-
-				/*
-				Account:
-					Account No.: 3
-					Account Name: General Credit Line
-					Client ID: 103
-					Account Balance: -777.0
-					Joint Account: no
-					Joint ID: N/A
-					Open Date: 2021-09-06
-					Close Date: null
-				Account:
-					Account No.: 2
-					Account Name: General Savings Account
-					Client ID: 102
-					Account Balance: 10000.0
-					Joint Account: no
-					Joint ID: N/A
-					Open Date: 2021-09-06
-					Close Date: null
-				Account:
-					Account No.: 1
-					Account Name: General Checking Account
-					Client ID: 101
-					Account Balance: 5500.0
-					Joint Account: no
-					Joint ID: N/A
-					Open Date: 2021-09-06
-					Close Date: null
-					Overdraft Protection: no
-				 */
-				
 		
 
 		/************** DRIVER IS HERE **************/
@@ -97,15 +65,10 @@ public class BankAccountDriver {
 			{
 				Credit creditAcc = (Credit) childrenAccounts[i];
 				System.out.println("\n\nThe current array member " + i  + ": is of type of Credit account ");
-				try {
-					if (creditAcc.equals(childrenAccounts[i])) 
-					{
-						System.out.println("CreditAcc " + creditAcc.toString() + " and array member "  + childrenAccounts[i].toString() + "are the same." );
-					}
-				}
-				catch (Exception e)
+				if (creditAcc.equals(childrenAccounts[i])) 
 				{
-					System.out.println("\n***\nError: Equals method requires closeDate member not to be null\n" + e.getMessage());
+					System.out.println("Following CreditAcc:\n " + creditAcc.toString() + "\n\n and array member:\n\n "
+				        + childrenAccounts[i].toString() + "\n\nare equal." );
 				}
 			}
 			
@@ -121,104 +84,9 @@ public class BankAccountDriver {
 			
 		}
 		
-		/*                      **** OUTPUT ****
-		 * 
-		    The current array member 0: is of type of Checking account 
-			Current balance on your checkingAccount: 5500.0
-			Withdrawal amount: -1.00000000099E9. Withdrawing...
-			Insufficient funds
-			Current balance of checking Account: 5500.0
-			
-			
-			The current array member 1: is of type of Savings account 
-			Savings account balance: 10000.0 before deposit.
-			Current balance: 11000.0
-			Savings balance after 1000.00 deposit : 11000.0
-			
-			
-			The current array member 2: is of type of Credit account 
-			
-			***
-			Error: Equals method requires closeDate member not to be null
-			Cannot invoke "java.time.LocalDate.equals(Object)" because the return value of "Account.getClose()" is null
-
-		 */
 	}
 	
-	//******************** DRIVER() *******************************
-	/*
-	 * Driver method
-	 */
-	private static void driver(Account ... childrenAccounts) 
-	{
-		for (int i = 0; i < childrenAccounts.length; i++)
-		{
-			if (childrenAccounts[i] instanceof Checking)
-			{
-				Checking checkingAcc = (Checking) childrenAccounts[i];
-				System.out.println("\n\nThe current array member " + i  + ": is of type of Checking account ");
-				
-				if (!checkingAcc.isOverdraftProtection()) 
-				{
-					System.out.println("Current balance on your checkingAccount: " + checkingAcc.getBalance());
-					double withdrawalAmount = -1000000000.99;
-					System.out.println("Withdrawal amount: " + withdrawalAmount + ". Withdrawing...");
-					checkingAcc.withdrawal(withdrawalAmount);
-					System.out.println("Current balance of checking Account: " + checkingAcc.getBalance());
-				}
-			}
-			
-			if (childrenAccounts[i] instanceof Credit)
-			{
-				Credit creditAcc = (Credit) childrenAccounts[i];
-				System.out.println("\n\nThe current array member " + i  + ": is of type of Credit account ");
-				try {
-					if (creditAcc.equals(childrenAccounts[i])) 
-					{
-						System.out.println("CreditAcc " + creditAcc.toString() + " and array member "  + childrenAccounts[i].toString() + "are the same." );
-					}
-				}
-				catch (Exception e)
-				{
-					System.out.println("\n***\nError: Equals method requires closeDate member not to be null\n" + e.getMessage());
-				}
-			}
-			
-			if (childrenAccounts[i] instanceof Savings)
-			{
-				Savings savingsAcc = (Savings) childrenAccounts[i];
-				System.out.println("\n\nThe current array member " + i  + ": is of type of Savings account ");
-				
-				System.out.println("Savings account balance: "  + savingsAcc.getBalance() + " before deposit.");
-				savingsAcc.deposit(1000.00);
-				System.out.println("Savings balance after 1000.00 deposit : " + savingsAcc.getBalance());
-			}
-			
-		}
-		
-		/*                      **** OUTPUT ****
-		 * 
-		    The current array member 0: is of type of Checking account 
-			Current balance on your checkingAccount: 5500.0
-			Withdrawal amount: -1.00000000099E9. Withdrawing...
-			Insufficient funds
-			Current balance of checking Account: 5500.0
-			
-			
-			The current array member 1: is of type of Savings account 
-			Savings account balance: 10000.0 before deposit.
-			Current balance: 11000.0
-			Savings balance after 1000.00 deposit : 11000.0
-			
-			
-			The current array member 2: is of type of Credit account 
-			
-			***
-			Error: Equals method requires closeDate member not to be null
-			Cannot invoke "java.time.LocalDate.equals(Object)" because the return value of "Account.getClose()" is null
 
-		 */
-	}
 
 
 }
